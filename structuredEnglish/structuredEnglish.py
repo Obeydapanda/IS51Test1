04e45f8ff99be641c64c7431dfe58d7efdc27227
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-11-12 16:52:57 -0800

    structured english end

---
+++ structuredEnglish/structuredEnglish.py
@@ -0,0 +1,14 @@
+"""
+This program will tell us if working for $100 a day versus $1
+a day that doubles each time with every day that we worked 
+will net us more money. We will use 2 functions to find this out
+then compare them and tell us which one is worth more.
+
+function1 will be 100 * 10 days
+function2 will loop 10 times, doulbing each time
+
+if the amount is equal it will say "Both options are the same"
+if Option 1 pays more it will say "Option 1 pays more"
+if Option 2 pays more it will say "Option 2 pays more"
+
+"""
